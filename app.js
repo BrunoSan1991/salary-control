f6ca28b9d9cb3a70b86d68ef6a641c6e42f973f7
Bruno Sanches <bsan.designgrafico@gmail.com>
2023-03-12 19:50:57 -0300

    adicionando atualizações

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const express = require('express');
 const mysql = require('mysql');
 const moment = require('moment');
 const path = require('path');
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
 
 const app = express();
 const port = 3000;
 
+// Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
+// Conexão com o banco de dados
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -19,9 +24,14 @@ const connection = mysql.createConnection({
 connection.connect((err) => {
   if (err) {
     console.error('Erro ao conectar ao banco de dados:', err);
-  } else {
-    console.log('Conexão com o banco de dados estabelecida com sucesso!');
+    return;
   }
+  console.log('Conexão bem-sucedida ao banco de dados!');
+});
+
+// Rotas
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.post('/produtos', (req, res) => {
@@ -41,15 +51,11 @@ app.post('/produtos', (req, res) => {
   });
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.get('/estoque/:id', (req, res) => {
+  const id = req.params.id;
 
-app.get('/estoque/:produto', (req, res) => {
-  const produto = req.params.produto;
-
-  const sql = `SELECT * FROM estoque WHERE nome = ?`;
-  const values = [produto];
+  const sql = `SELECT * FROM produtos WHERE id = ?`;
+  const values = [id];
 
   connection.query(sql, values, (err, result) => {
     if (err) {
@@ -58,7 +64,7 @@ app.get('/estoque/:produto', (req, res) => {
     }
 
     if (result.length === 0) {
-      return res.send(`O produto "${produto}" não foi encontrado no estoque.`);
+      return res.send(`O produto com o ID "${id}" não foi encontrado no estoque.`);
     }
 
     const dataValidade = moment(result[0].validade, 'YYYY-MM-DD');
@@ -66,14 +72,36 @@ app.get('/estoque/:produto', (req, res) => {
     const diasRestantes = dataValidade.diff(hoje, 'days');
 
     if (diasRestantes <= 0) {
-      return res.send(`O produto "${produto}" está vencido.`);
+      return res.send(`O produto "${result[0].nome}" está vencido.`);
     } else {
-      return res.send(`O produto "${produto}" tem ${result[0].quantidade} unidades e vence em ${diasRestantes} dias.`);
+      return res.send(`O produto "${result[0].nome}" tem ${result[0].quantidade} unidades disponíveis e vence em ${diasRestantes} dias.`);
     }
+  });
+});
+
+app.get('/relatorio', (req, res) => {
+  const sql = `SELECT p.*, e.quantidade
+               FROM produtos p
+               INNER JOIN estoque e ON p.nome = e.nome
+               WHERE p.nome LIKE '%pesquisa%'`;
+
+  connection.query(sql, (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Ocorreu um erro ao gerar o relatório!');
+    }
+
+    const doc = new PDFDocument();
+    doc.pipe(fs.createWriteStream('output.pdf'));
+
+    doc.text('Relatório de Produtos', { align: 'center', size: 20, underline: true });
+    doc.moveDown();
+
+    doc.end();
+    res.send('Relatório gerado com sucesso!');
   });
 });
 
 app.listen(port, () => {
   console.log(`Aplicativo web iniciado na porta ${port}!`);
 });
-
