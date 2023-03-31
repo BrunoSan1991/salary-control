c91c1166aab8f15439aa3156206afe4966486bfa
Bruno Sanches <bsan.designgrafico@gmail.com>
2023-03-30 22:09:18 -0300

    atualizações no parametro de busca dos produtos

--- app.js
+++ app.js
@@ -35,27 +35,38 @@ app.get('/', (req, res) => {
 });
 
 app.post('/produtos', (req, res) => {
-  const { codigo, nome, quantidade, validade } = req.body;
+  const { codigo, nome, quantidade, validade, marca_do_produto } = req.body;
 
-  const sql = `INSERT INTO produtos (codigo, nome, quantidade, validade) VALUES (?, ?, ?, ?)`;
-  const values = [codigo, nome, quantidade, validade];
+  const sql = `INSERT INTO produtos (codigo, nome, quantidade, validade, marca_do_produto) VALUES (?, ?, ?, ?, ?)`;
+  const values = [codigo, nome, quantidade, validade, marca_do_produto];
 
   connection.query(sql, values, (error, results, fields) => {
     if (error) {
       console.error("Erro ao inserir registro:", error.message);
-      res.status(500).send('Erro ao inserir registro.');
+      res.status(500).send("<script>alert('Erro ao inserir registro'); window.location.href='/';</script>");
       return;
     }
     console.log("Registro inserido com sucesso!");
-    res.send('Registro inserido com sucesso!');
+    res.send("<script>alert('Registro inserido com sucesso!'); window.location.href='/';</script>");
+    
   });
 });
 
-app.get('/estoque/:id', (req, res) => {
-  const id = req.params.id;
+app.get('/estoque/:parametro', (req, res) => {
+  const parametro = req.params.parametro;
 
-  const sql = `SELECT * FROM produtos WHERE id = ?`;
-  const values = [id];
+  let sql;
+  let values;
+
+  if (isNaN(parametro)) {
+    // O parâmetro não é um número, então é o nome do produto
+    sql = `SELECT * FROM produtos WHERE nome = ?`;
+    values = [parametro];
+  } else {
+    // O parâmetro é um número, então é o ID do produto
+    sql = `SELECT * FROM produtos WHERE id = ?`;
+    values = [parametro];
+  }
 
   connection.query(sql, values, (err, result) => {
     if (err) {
@@ -64,7 +75,7 @@ app.get('/estoque/:id', (req, res) => {
     }
 
     if (result.length === 0) {
-      return res.send(`O produto com o ID "${id}" não foi encontrado no estoque.`);
+      return res.send(`O produto com o parâmetro "${parametro}" não foi encontrado no estoque.`);
     }
 
     const dataValidade = moment(result[0].validade, 'YYYY-MM-DD');
